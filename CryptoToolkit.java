dcb6c9fe816fb8edd694181a350b4822540c7020
Nguyen The Sang <0384589559sang@gmail.com>
2026-04-17 21:40:47 +0700

    Nop bai 3DES

---
+++ CryptoToolkit.java
@@ -0,0 +1,33 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Scanner;
+
+public class CryptoToolkit {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("=== 3DES ENCRYPTION TOOL ===");
+            System.out.print("Nhap van ban can ma hoa: ");
+            String plainText = sc.nextLine();
+
+            KeyGenerator keygen = KeyGenerator.getInstance("DESede");
+            SecretKey key = keygen.generateKey();
+            System.out.println("\n[+] Da tao khoa 3DES thanh cong.");
+
+            Cipher cipher = Cipher.getInstance("DESede");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+            String encodedText = Base64.getEncoder().encodeToString(encryptedBytes);
+            System.out.println("[+] Ban ma (Base64): " + encodedText);
+            
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encodedText));
+            System.out.println("[+] Giai ma thanh cong: " + new String(decryptedBytes));
+            
+        } catch (Exception e) {
+            System.out.println("Loi: " + e.getMessage());
+        }
+    }
+}
